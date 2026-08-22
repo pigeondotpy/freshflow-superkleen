fa1b35ef1a5e908bfbe9c0a7a1f1b1488cbbc93f
pigeondotpy <43497651+pigeondotpy@users.noreply.github.com>
2026-08-22 12:01:35 +0200

    Add FreshFlow Supabase client configuration

---
+++ config.js
@@ -0,0 +1,5 @@
+window.FRESHFLOW_CONFIG = {
+  SUPABASE_URL: 'https://qyqokcayqtapiaktszbv.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_9wQObf0XPDjW2_Mcv69Qrg_Pmzwd4-D',
+  DEFAULT_BUSINESS_NAME: 'Superkleen Dry Cleaners'
+};

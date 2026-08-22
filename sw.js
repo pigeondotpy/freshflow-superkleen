bbf5bcb70656a73612c566c6bc9f447df8bf827b
pigeondotpy <43497651+pigeondotpy@users.noreply.github.com>
2026-08-22 16:56:58 +0200

    Add FreshFlow PWA service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='freshflow-v06-shell';
+const SHELL=['/','/index.html','/config.js','/manifest.webmanifest'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(r=>{let copy=r.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return r}).catch(()=>caches.match(event.request).then(r=>r||caches.match('/index.html'))))});
